b2b03007988b485d62bf9a74a6f41db6f385b29f
Yuyn1 <adityahertanto1012@gmail.com>
2026-06-13 22:48:49 +0700

    Update Admin.java

--- UAS PBO/model/Admin.java
+++ UAS PBO/model/Admin.java
@@ -1,35 +1,33 @@
 package model;
 
-public class admin {
-    private int idAdmin;
+public class Admin {
     private String username;
     private String password;
 
-    public admin(){};
+    // 1. Constructor Kosong (Ini yang dipanggil oleh AdminDAOImpl lewat 'new Admin()')
+    public Admin() {
+    }
 
-    public admin(int idAdmin, String username, String password){
-        this.idAdmin = idAdmin;
+    // 2. Constructor dengan Parameter (Opsional, jika sewaktu-waktu kamu butuh)
+    public Admin(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    public int getId() {
-        return idAdmin;
-    }
-    public void setId(int idAdmin) {
-        this.idAdmin = idAdmin;
-    }
-
+    // --- GETTER & SETTER USERNAME ---
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
 
+    // --- GETTER & SETTER PASSWORD ---
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
